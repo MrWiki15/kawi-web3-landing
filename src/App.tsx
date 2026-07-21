a7b1b61a45b279025eebbd05e3a7261515c78982
Wilkenson <wikicanton439@gmail.com>
2026-07-21 08:58:18 -0400

    Add app route redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Index from "@/pages/Index";
+
+const KAWI_APP_URL = "https://kawiservices.com.br/app";
+
+const ExternalAppRedirect = () => {
+  useEffect(() => {
+    window.location.replace(KAWI_APP_URL);
+  }, []);
+
+  return null;
+};
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Index />} />
+    <Route path="/app" element={<ExternalAppRedirect />} />
+    <Route path="/app/*" element={<ExternalAppRedirect />} />
     <Route path="*" element={<Navigate to="/" replace />} />
   </Routes>
 );
